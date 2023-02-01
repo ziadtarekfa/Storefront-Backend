5a8661a4de1494a184a976a2a984678ec79f5299
Ziad Tarek <ziadtarekfa@gmail.com>
2023-02-01 17:18:33 +0200

    implemented  database configuration file

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const client = new Pool({
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+});
+
+export default client;
