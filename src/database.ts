ae74cd8eb640927266b8fe87c13f8e62b3ca5c66
Ziad Tarek <ziadtarekfa@gmail.com>
2023-02-09 02:28:04 +0200

    implemented test environment

--- src/database.ts
+++ src/database.ts
@@ -3,11 +3,22 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-const client = new Pool({
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD
-});
-
+let databaseConfiguration;
+if (process.env.ENV == 'dev') {
+    databaseConfiguration = {
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD
+    };
+}
+else if (process.env.ENV == 'test') {
+    databaseConfiguration = {
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DB_TEST,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD
+    };
+}
+const client = new Pool(databaseConfiguration);
 export default client;
